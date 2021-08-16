613ac6dd44fefeb6753c3ae3bf8b14f8737fec0a
withJack <31638192+withJack@users.noreply.github.com>
2021-08-16 23:19:11 +0900

    programmers 42885

---
+++ 42885_lifeboat.py
@@ -0,0 +1,19 @@
+import heapq
+
+def solution(people, limit):
+    answer = 0
+    
+    people.sort()
+    i, j = 0, len(people)-1
+    while i <= j:
+        if i == j:
+            answer += 1
+            return answer
+        if people[i] + people[j] <= limit:
+            i += 1
+            j -= 1
+        else:
+            j -= 1
+        answer += 1
+    
+    return answer
